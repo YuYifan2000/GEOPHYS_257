9b5fbf6938e6b6ae4cefd1b905d954c0d5f78ae5
Yifan <dxyuyifan@gmail.com>
2023-01-13 11:21:18 -0800

    debug prog.py

---
+++ prog.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+
+
+def printMany(word:str,n:int):
+  """ I print word n times"""
+  for i in range(0,n):
+    print(word)
+
+
+
+
+if __name__ == "__main__":
+  printMany("Hello world",10)
